016b852ffeeed27394f022da0c59111eba23ea8e
chanmob <sellingout@naver.com>
2021-01-31 00:08:25 +0900

    타이틀 추가

---
+++ LightHouse/Assets/Chan/Scripts/Title.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    private float elapsedtime = 0;
+    [SerializeField]
+    private float waitTime;
+
+    private void Start()
+    {
+        StartCoroutine(TitleCoroutine());
+    }
+
+    private IEnumerator TitleCoroutine()
+    {
+        Vector3 startPos = new Vector3(0, 13.5f, -10);
+        Vector3 endPos = new Vector3(0, -13.5f, -10);
+
+        while(elapsedtime < waitTime)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, elapsedtime / waitTime);
+            elapsedtime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPos;
+    }
+
+    public void LoadInGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("InGame");
+    }
+}
